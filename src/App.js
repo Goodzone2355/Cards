6011f6a71510b15befa42a2264f05384423e63e2
Goodzone2355 <chulskij1996@gmail.com>
2023-06-15 23:33:57 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './Card';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h2 className='container__h2'>Хиты продаж</h2>
+      
+      <div className='cards'>
+        <Card 
+          name = ' FOREVER X880 29" (2022) Grey-Red'
+          pay = {194135} 
+          src = '/img/Rectangle.png'
+          sale = {false}
+        />
+
+        <Card 
+          name = ' GRIZZILY MOUNTAINE BIKE 29" (2022) White'
+          pay = {41000} 
+          src = '/img/Rectangle1.png'
+          sale = {true}  
+        />
+
+        <Card 
+          name = 'FORWARD DORTMUND 28 1.0 (2022) черный/бронзовый'
+          pay = {50000}   
+          src = '/img/Rectangle2.png'
+          sale = {false}
+        />
+
+        <Card 
+          name = 'STINGER ELEMENT 24" (2022) зеленый'
+          pay = {25000}   
+          src = '/img/Rectangle3.png'
+          sale = {true}
+        />
+
+        <Card 
+          name = ' FOREVER X880 29" (2022) Grey-Red'
+          pay = {194135}  
+          src = '/img/Rectangle.png' 
+          sale = {false}
+        />
+
+        <Card 
+          name = ' GRIZZILY MOUNTAINE BIKE 29" (2022) White'
+          pay = {41000}   
+          src = '/img/Rectangle1.png'  
+          sale = {false}
+        />
+
+        <Card 
+          name = 'FORWARD DORTMUND 28 1.0 (2022) черный/бронзовый'
+          pay = {50000}   
+          src = '/img/Rectangle2.png'
+          sale = {false}
+        />
+
+        <Card 
+          name = 'STINGER ELEMENT 24" (2022) зеленый'
+          pay = {25000}   
+          src = '/img/Rectangle3.png'
+          sale = {true}
+        />
+      </div>
     </div>
   );
 }
